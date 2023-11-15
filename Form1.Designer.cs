33a2bad8d1d9242ce09a0a9bfb39aae0743274e5
Ru1vly <67930426+Ru1vly@users.noreply.github.com>
2023-11-15 13:38:30 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RSA_Security_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button DecryptButton;
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            DecryptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DecryptButton
+            // 
+            DecryptButton.Location = new System.Drawing.Point(382, 182);
+            DecryptButton.Name = "DecryptButton";
+            DecryptButton.Size = new System.Drawing.Size(75, 23);
+            DecryptButton.TabIndex = 3;
+            DecryptButton.Text = "Decrypt";
+            DecryptButton.UseVisualStyleBackColor = true;
+            DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(318, 71);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 50);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(318, 127);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(100, 50);
+            this.OutputTextBox.TabIndex = 1;
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(291, 182);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(75, 23);
+            this.EncryptButton.TabIndex = 2;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.Location = new System.Drawing.Point(318, 226);
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.KeyTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.KeyTextBox);
+            this.Controls.Add(DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.TextBox KeyTextBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSA_Security_System
+{
+    public partial class Form1 : Form
+    {
+        private string key = "3356249871195716"; // 16 characters for 128 bits
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Implement your logic for the textBox1_TextChanged event, if needed
+        }
+
+        private void KeyTextBox_TextChanged(object sender, EventArgs e)
+        {
+            key = KeyTextBox.Text;
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string encryptedFilePath = Path.Combine(Path.GetDirectoryName(filePath), "encrypted_" + Path.GetFileName(filePath));
+
+                EncryptFile(filePath, encryptedFilePath);
+
+                MessageBox.Show("File encrypted successfully!");
+            }
+        }
+
+        private void EncryptFile(string inputFilePath, string outputFilePath)
+        {
+            using (RijndaelManaged rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = Encoding.UTF8.GetBytes(key);
+                rijndael.Mode = CipherMode.ECB;
+
+                ICryptoTransform encryptor = rijndael.CreateEncryptor(rijndael.Key, rijndael.IV);
+
+                using (FileStream inputFileStream = new FileStream(inputFilePath, FileMode.Open))
+                using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create))
+                using (CryptoStream cryptoStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write))
+                {
+                    inputFileStream.CopyTo(cryptoStream);
+                }
+            }
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string decryptedFilePath = Path.Combine(Path.GetDirectoryName(filePath), "decrypted_" + Path.GetFileName(filePath));
+
+                DecryptFile(filePath, decryptedFilePath);
+
+                MessageBox.Show("File decrypted successfully!");
+            }
+        }
+
+        private void DecryptFile(string inputFilePath, string outputFilePath)
+        {
+            using (RijndaelManaged rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = Encoding.UTF8.GetBytes(key);
+                rijndael.Mode = CipherMode.ECB;
+
+                ICryptoTransform decryptor = rijndael.CreateDecryptor(rijndael.Key, rijndael.IV);
+
+                using (FileStream inputFileStream = new FileStream(inputFilePath, FileMode.Open))
+                using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create))
+                using (CryptoStream cryptoStream = new CryptoStream(outputFileStream, decryptor, CryptoStreamMode.Write))
+                {
+                    inputFileStream.CopyTo(cryptoStream);
+                }
+            }
+        }
+    }
+}
